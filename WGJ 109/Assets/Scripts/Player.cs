163e35360fbe4523ce904d93dfb50e71af261a39
5n1p3r_4rt3m1s <skamickrahaman12@gmail.com>
2019-08-11 22:41:07 +0530

    Camera and Character Controls
    A/D - Move
    Space - Jump
    
    [placeholder character and ground added for testing]

---
+++ WGJ 109/Assets/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float spd = 10f;
+    public float jumpH = 5f;
+    public Collider2D groundCheck;
+
+    private Rigidbody2D rb;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A)) 
+		{ 
+			rb.AddForce(new Vector3(-1f, 0f) * spd);
+			//Play animation
+		}		
+        else if (Input.GetKey(KeyCode.D)) 
+		{ 
+			rb.AddForce(new Vector3(1f, 0f) * spd); 
+			//Play animation
+		}
+
+        Debug.Log("Grounded: " + groundCheck.IsTouchingLayers());
+
+        if (Input.GetKeyDown(KeyCode.Space) && groundCheck.IsTouchingLayers())
+            { 
+				rb.AddForce(new Vector3(0f, 2f) * jumpH); 
+				//Anim
+			}
+    }
+}
